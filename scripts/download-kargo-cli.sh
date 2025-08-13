acbc3b74326c6916c8dc97d34413447996050d00
thatmlopsguy <165834479+thatmlopsguy@users.noreply.github.com>
2025-08-13 17:36:22 +0100

    feat: Enhance Kargo CLI installation script with version check and success message

--- scripts/download-kargo-cli.sh
+++ scripts/download-kargo-cli.sh
@@ -3,4 +3,12 @@
 arch=$(uname -m)
 [ "${arch}" = "x86_64" ] && arch=amd64
 curl -L -o kargo https://github.com/akuity/kargo/releases/latest/download/kargo-"$(uname -s | tr '[:upper:]' '[:lower:]')-${arch}"
+
 chmod +x kargo
+
+sudo mv ./kargo /usr/local/bin/kargo
+
+# print kargo version
+kargo version --client
+
+echo "Kargo CLI installed successfully."
